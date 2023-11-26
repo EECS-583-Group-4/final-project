c73e2d3a8dae8848a494afb2f28b4412ddded905
Ivris Raymond <theivris@gmail.com>
2023-11-26 13:59:38 -0500

    Revert "First commit, going to need to add recursion for U-D chain next"
    This reverts commit ddbf3365b2781b5506d8f8868d7a2e131631d712.

--- MAS/MASPass/CMakeFiles/install-MASPass-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-MASPass-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-MASPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
