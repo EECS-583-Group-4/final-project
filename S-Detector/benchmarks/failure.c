b3c6097919795fc0a1fedf4db633944b284e31d2
Luke Lesh <leshlu@eecs583a.eecs.umich.edu>
2023-12-12 16:15:45 -0500

    commented benchmark

--- S-Detector/benchmarks/failure.c
+++ S-Detector/benchmarks/failure.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 #define SIZE 1000
-#define OFFSET 7
-#define INCREMENT 3
 
 int main()
 {
     int a[SIZE] = {0};
     int b[SIZE * 2] = {0};
 
-    for (int i = OFFSET; i < SIZE + OFFSET; i *= INCREMENT)
+    // i ranges from 7 to 567
+    for (int i = 7; i < SIZE + 7; i *= 3)
     {
-        printf("%d \n", a[(i - OFFSET) * 2]);
+        // array index ranges from 0 to 1120
+        printf("%d \n", a[(i - 7) * 2]);
     }
     return 0;
 }
