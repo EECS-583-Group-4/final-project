c1b96107ca9e718079d112c4ad43fc3a59b71e4d
Luke Lesh <leshlu@eecs583a.eecs.umich.edu>
2023-12-12 12:54:38 -0500

    generate basic cfg for presentation

--- S-Detector/MASBasedPass/tests/test3.c
+++ S-Detector/MASBasedPass/tests/test3.c
@@ -2,9 +2,11 @@
 
 // test_loop_out_of_bound.c from Spindle
 
-int main() {
+int main()
+{
     int a[10] = {0};
-    for (int i = 0; i < 11; ++i) {
+    for (int i = 0; i < 10; ++i)
+    {
         printf("%d \n", a[i]);
     }
     return 0;
